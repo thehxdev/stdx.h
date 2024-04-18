28888e8cb9450b30e18662ee99ff1b28c8c361d5
thehxdev <hossein.khosravi.ce@gmail.com>
2024-04-18 12:24:10 +0330

    testing append_many macro

--- test/test.c
+++ test/test.c
@@ -4,10 +4,10 @@
 
 int main(void) {
     size_t i;
+    int nums[5] = { 1, 2, 3, 4, 5, };
     Stdx_DA darr = stdx_da_new(10);
 
-    for (i = 0; i < 10; i++)
-        stdx_da_append(&darr, &i, int);
+    stdx_da_append_many(&darr, nums, int, STDX_ARRAY_LEN(nums));
 
     for (i = 0; i < darr.len; i++)
         printf("%d\n", *stdx_da_get(&darr, int, i));
