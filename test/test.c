84114f51acd5f6ab23f3475d8707daa9ef8a9c0d
thehxdev <hossein.khosravi.ce@gmail.com>
2024-04-19 17:17:46 +0330

    testing dynamic array

--- test/test.c
+++ test/test.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "../src/stdx.h"
 
 int main(void) {
     size_t i;
-    int nums[5] = { 1, 2, 3, 4, 5, };
-    Stdx_DA darr = stdx_da_new(10);
+    Stdx_DArr nums = stdx_da_new(20);
 
-    stdx_da_append_many(&darr, nums, int, STDX_ARRAY_LEN(nums));
+    for (i = 0; i < 20; i++)
+        stdx_da_append(&nums, size_t, &i);
 
-    for (i = 0; i < darr.len; i++)
-        printf("%d\n", *stdx_da_get(&darr, int, i));
+    for (i = 0; i < nums.len; i++)
+        printf("%zu ", *stdx_da_get(&nums, size_t, i));
+    printf("\n");
 
-    stdx_da_free(&darr);
+    stdx_da_free(nums);
     return 0;
 }
