7ad60d7131ace73349d6444d1a4122b86e956ece
thehxdev <hossein.khosravi.ce@gmail.com>
2024-05-02 19:39:11 +0330

    test dynamic array

--- test/test.c
+++ test/test.c
@@ -1,19 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "../src/stdx.h"
 
 
+void test_stdx_darr(void);
 void test_stdx_parse_long(void);
 void test_stdx_parse_long_all(void);
 
 
 int main(void) {
+    test_stdx_darr();
     test_stdx_parse_long();
     test_stdx_parse_long_all();
 
     STDX_LOG_INF("All tests are PASSED!\n", NULL);
     return 0;
+}
+
+
+void test_stdx_darr(void) {
+    Stdx_DArr tmp = stdx_da_new(20);
+    STDX_ASSERT(tmp.len == 0);
+    STDX_ASSERT(tmp.cap == 20);
+    STDX_ASSERT(tmp.items == NULL);
+
+    long num = 10;
+
+    stdx_da_append(&tmp, long, &num);
+    STDX_ASSERT((*stdx_da_get(&tmp, long, 0)) == 10);
+
+    num = 873;
+    stdx_da_append(&tmp, long, &num);
+    STDX_ASSERT((*stdx_da_get(&tmp, long, 1)) == 873);
+
+    stdx_da_free(tmp);
+    STDX_ASSERT(tmp.len == 0);
+    STDX_ASSERT(tmp.items == NULL);
+
+    stdx_da_append_many(&tmp, char, "Hello World!", strlen("Hello World!"));
+    STDX_ASSERT(strcmp(stdx_da_ptr(&tmp, char), "Hello World!") == 0);
+    stdx_da_free(tmp);
+
+    STDX_LOG_INF("PASSED...\n", NULL);
 }
 
 
@@ -44,7 +74,7 @@ void test_stdx_parse_long() {
     tmp = stdx_parse_long("foo453bar");
     STDX_ASSERT(tmp == 453);
 
-    STDX_LOG_INF("stdx_parse_long PASSED...\n", NULL);
+    STDX_LOG_INF("PASSED...\n", NULL);
 }
 
 
@@ -68,5 +98,5 @@ void test_stdx_parse_long_all(void) {
                 && (*stdx_da_get(&tmp, long, 2)) == -1);
     stdx_da_free(tmp);
 
-    STDX_LOG_INF("stdx_parse_long_all PASSED...\n", NULL);
+    STDX_LOG_INF("PASSED...\n", NULL);
 }
